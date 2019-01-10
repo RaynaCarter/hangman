1c53b3b788d5047b8cc2b0e947173d2e714a71e9
Rayna <Babygirl@administrators-MacBook-Pro.local>
2019-01-10 07:35:27 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Rayna Carter
+ * Hangman Project
+ * Dec 28, 2018
+ */
+var state =  document.getElementById("category");
+var STATES = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana",
+    "Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio",
+    "Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+
+var inputLetter = document.getElementById("guessbox").value;
+var letterArray = []
+function categoryChosen(x){
+    if (x === "U.S States"){
+
+    }
+
+
+}
