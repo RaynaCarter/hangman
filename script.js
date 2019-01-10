bb0e2499ffe83cd0676be565803e3549f05cfbf6
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-01-10 13:24:02 -0800

    //

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ var STATES = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","C
     "Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio",
     "Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 
-var inputLetter = document.getElementById("guessbox").value;
-var letterArray = []
+//var inputLetter = document.getElementById("guessbox").value;
+var word = "";
+var letterArray = [];
+var guessedetters = [];
+function underlines(){
+    document.getElementById("word").innerHTML = word;
+}
 function categoryChosen(x){
-    if (x === "U.S States"){
-
+    var choice = document.getElementById("category").value;
+    if (choice == 1){
+         word = STATES[Math.floor(Math.random()* STATES.length)];
     }
-
-
+    console.log(word);
 }
