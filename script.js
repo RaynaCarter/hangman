da4b63ce6706938257e2803c079447b04be3ef96
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-01-11 13:02:01 -0800

    //

--- script.js
+++ script.js
@@ -7,17 +7,35 @@ var STATES = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","C
     "Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio",
     "Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 
-//var inputLetter = document.getElementById("guessbox").value;
+var numUnderLines = 0;
+var inputLetter = document.getElementById("guessbox").value;
 var word = "";
 var letterArray = [];
 var guessedetters = [];
-function underlines(){
-    document.getElementById("word").innerHTML = word;
-}
+var answer= [];
+
 function categoryChosen(x){
     var choice = document.getElementById("category").value;
     if (choice == 1){
          word = STATES[Math.floor(Math.random()* STATES.length)];
     }
     console.log(word);
+}
+
+function underline() {
+    for (var i = 0; i < word.length; i++) {
+        numUnderLines += 1;
+    }
+    return numUnderLines;
+    document.getElementById("underline").innerHTML= numUnderLines;
+
+}
+
+function printWord(){
+    for(var i=0;i< word.length;i++){
+        if(word[i]= inputLetter){
+            answer+= word[i]
+        }
+    }
+    return answer;
 }
